4b53ad54a547b129c23e0c07fc80657391d6f6fe
Mateus Feliciano <86882032+Leykon7@users.noreply.github.com>
2022-10-05 20:00:39 -0300

    Modificação na forma de transferência

---
+++ Exercicio-do-Sistema-Bancario-main/inc/banco.h
@@ -0,0 +1,21 @@
+#ifndef BANCO_H
+#define BANCO_H
+
+#include "conta.h"
+
+#define NUMCONTAS 100 //Define que a palavar NUMCONTAS passa a valer como escrever 100
+
+class Banco
+{
+private:
+    Conta contas[NUMCONTAS];//Cria um vetor de objetos do tipo Conta que pode armazenar até 100 contas
+public:
+    Banco();
+    ~Banco();
+    Conta* buscaConta(int numero); //Metodo que retorna o endereço do objeto conta que possui o mesmo numero informado
+    void atendimento();
+    void criadorConta(int senhain, int i, std::string nome, std::string tipo, double saldo);
+};
+
+
+#endif
